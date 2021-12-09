36b70e8babe4b5ee1837116dea79fbc4a29e7114
quirkee.admin <quirkee.group@gmail.com>
2021-12-09 22:25:04 +0800

    upgrade to 2.8.0

---
+++ lib/app.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+import './theme.dart';
+import './page/home.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: QuirkeeThemeData.lightThemeData,
+      darkTheme: QuirkeeThemeData.darkThemeData,
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('en', ''),
+        Locale('zh', ''),
+      ],
+      home: const HomePage(),
+    );
+  }
+}
+
